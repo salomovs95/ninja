15877e7eea1a8fbaacacea34bf7585e3c0e15b70
Salomao S. <souza95salomao@gmail.com>
2026-06-06 22:44:01 +0000

    :construction: fix: fixes missing model id generation strategy

--- src/main/java/com/salomovs/ninjacouncil/model/NinjaMission.java
+++ src/main/java/com/salomovs/ninjacouncil/model/NinjaMission.java
@@ -8,6 +8,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -26,6 +28,7 @@ import lombok.Setter;
 @NoArgsConstructor
 public class NinjaMission {
   @Id
+  @GeneratedValue(strategy=GenerationType.IDENTITY)
   private Long id;
 
   @ManyToOne(fetch=FetchType.LAZY)
